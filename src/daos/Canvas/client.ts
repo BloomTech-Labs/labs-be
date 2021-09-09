1dbfac957c94d1ba1cc8f483cc0f67a4064a3ce5
Jake Mallory <4095+tinomen@users.noreply.github.com>
2021-09-08 23:20:31 -0600

    update eslint config fix linting issues

--- src/daos/Canvas/client.ts
+++ src/daos/Canvas/client.ts
@@ -1,40 +1,44 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export interface IClanvasClient {
-  get: (path: string) => any | null;
+  get: (path: string) => Promise<any | null>;
 }
 
 class CanvasClient {
-  
-  public options: any;
-  public headers: any;
+  public options: Record<string, unknown>;
+  public headers: Record<string, unknown>;
 
-  constructor(opts: any) {
-    if(!opts.token) { throw new Error("missing canvas api token option") }
-    this.options =  Object.assign({
-      base_url: 'https://lambdaschool.instructure.com/api/v1/',
-      ver: "/v1"
-    }, opts);
-    this.headers = {
-      "Authorization": `Bearer ${opts.token}`,
-      "Content-Type": "application/vnd.api+json",
-      "Accept": "application/vnd.api+json"
+  constructor(opts: Record<string, unknown> = {}) {
+    if (!opts.token) {
+      throw new Error("missing canvas api token option");
     }
+    this.options = Object.assign(
+      {
+        base_url: "https://lambdaschool.instructure.com/api/v1/",
+        ver: "/v1",
+      },
+      opts
+    );
+    this.headers = {
+      Authorization: `Bearer ${opts.token}`,
+      "Content-Type": "application/vnd.api+json",
+      Accept: "application/vnd.api+json",
+    };
   }
 
-  public async request(path: string = "", options = {}) {
-    const url = this.options.base_url + path
+  public async request(path = "", options: unknown = {}): Promise<any> {
+    const url = `${this.options.base_url}${path}`;
 
     const config = {
       ...options,
-      headers: this.headers
-    }
+      headers: this.headers,
+    };
 
     const res = await fetch(url, config);
     return await res.json();
   }
 
-  public get(path: string): any {
+  public get(path: string): Promise<any> {
     return this.request(path);
   }
 }
