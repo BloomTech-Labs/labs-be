7a8c9adeb21b3e1673d7f2a0c45b0f15c95b989c
Frank Fusco <26161410+frankfusco@users.noreply.github.com>
2021-09-11 15:03:34 -0400

    Moved Airtable base ID to env var, added check

--- src/daos/AirTable/CanvasCoursesDao.ts
+++ src/daos/AirTable/CanvasCoursesDao.ts
@@ -1,42 +1,57 @@
 import Airtable from 'airtable';
+import { AirtableBase } from 'airtable/lib/airtable_base';
 
 
 class CanvasCoursesDao {
 
-    base_id = 'appvMqcwCQosrsHhM';
-    api_key = process.env.AT_API_KEY;
-    airtable = new Airtable({apiKey: this.api_key}).base(this.base_id);
-  
-    /**
-     *
-     */
-    public async getAll(): Promise<any> {
-      const courses = await this.airtable('Labs - Courses').select({
-          view: "Grid view"
-      }).all();
+  private api_key: string;
+  private base_id: string;
+  public airtable: AirtableBase;
 
-      return courses;
+  constructor () {
+    if (!process.env.AT_API_KEY) {
+      throw new Error("Missing Airtable API key");
+    }
+    if (!process.env.AT_SMT_BASE_ID) {
+      throw new Error("Missing Airtable base ID");
     }
 
-    /**
-     *  @param cohort
-     */
-    public async getObjectiveCourseByRole(role : string): Promise<any> {
-      const courses = await this.airtable('Labs - Courses').select({
-          view: `Objective Courses`,
-          maxRecords: 1,
-          filterByFormula: `{Role} = "${role}"`,
-      }).all();
-
-      if (courses.length) {
-        if (courses [0].fields) {
-          return courses [0].fields ['Course ID'];
-        } 
-      }
-
-      return null; // TODO: Do we want to return null or [] ?
-    }
-
+    this.api_key = process.env.AT_API_KEY;
+    this.base_id = process.env.AT_SMT_BASE_ID;
+    this.airtable = new Airtable({apiKey: this.api_key}).base(this.base_id);
   }
+  
+
+  /**
+   *
+   */
+  public async getAll(): Promise<any> {
+    const courses = await this.airtable('Labs - Courses').select({
+        view: "Grid view"
+    }).all();
+
+    return courses;
+  }
+
+  /**
+   *  @param cohort
+   */
+  public async getObjectiveCourseByRole(role : string): Promise<any> {
+    const courses = await this.airtable('Labs - Courses').select({
+        view: `Objective Courses`,
+        maxRecords: 1,
+        filterByFormula: `{Role} = "${role}"`,
+    }).all();
+
+    if (courses.length) {
+      if (courses [0].fields) {
+        return courses [0].fields ['Course ID'];
+      } 
+    }
+
+    return null; // TODO: Do we want to return null or [] ?
+  }
+
+}
   
   export default CanvasCoursesDao;
