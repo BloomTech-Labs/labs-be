24ccd979cf44fefe64d10ae21571a12cd8617201
Jake Mallory <4095+tinomen@users.noreply.github.com>
2021-09-24 15:57:50 -0600

    extend express.Request with currentUser property

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,7 +1,8 @@
-declare global {
-  namespace Express {
-    export interface Request {
-      currentUser?: import("@entities/User").IUser | undefined;
-    }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Express } from "express-serve-static-core";
+
+declare module "express-serve-static-core" {
+  export interface Request {
+    currentUser?: import("@entities/User").IUser | undefined;
   }
 }
