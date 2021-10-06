959ade6d98d4e6c620b46f4c9e935be47b5b13fd
Jake Mallory <4095+tinomen@users.noreply.github.com>
2021-10-06 14:56:42 -0600

    add comment in type extension module @types/index

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,3 +1,6 @@
+// importing Express from the a-typical module in order to get the
+// express-serve-static-core namespace into this module. Nothing else
+// worked from our friend on the internet.
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Express } from "express-serve-static-core";
 
