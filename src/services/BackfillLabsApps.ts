185528bd5ceeb67cb898ed9c466aa2f6e41d922e
ashtilawat23 <ashtilawat23@gmail.com>
2022-09-02 18:00:02 -0400

    fixed types for time slot and app

--- src/services/BackfillLabsApps.ts
+++ src/services/BackfillLabsApps.ts
@@ -9,14 +9,56 @@ const surveyDao = new SurveyDao();
 export const backfillLabsApps = async () => {
   const records = await surveyDao.getAll();
   records.forEach(async (record) => {
-    const jdsTrackEnrollmentId =
-      await jdsTrackEnrollmentDao.getJdsTrackEnrollmentIdByLambdaId(
-        "sample Lambda ID"
-      );
+    const lambdaId = record["fields"]["Lambda Id"][0]? record["fields"]["Lambda Id"][0]: ""
+    const jdsTrackEnrollmentId = await jdsTrackEnrollmentDao.getJdsTrackEnrollmentIdByLambdaId(lambdaId);
     labsApplicationDao.postLabsApplication(
-      jdsTrackEnrollmentId, // jdsTrackEnrollmentId
-      {}, // labsTimeSlot
-      {} // labsApplication
+      jdsTrackEnrollmentId,
+      {
+        id: "",
+        labsProducts: [],
+        name: "",
+        shortName: "",
+        ownerId: "",
+        tracks: []
+      },
+      {
+        timeSlotChoice1: "",
+        timeSlotChoice2: "",
+        timeSlotChoice3: "",
+        timeSlotChoice4: "",
+        gitHubHandle: "",
+        gitExpertise: 0,
+        dockerExpertise: 0,
+        playByEar: 0,
+        detailOriented: 0,
+        speakUpInDiscussions: 0,
+        soloOrSocial: "",
+        meaningOrValue: "",
+        feelsRightOrMakesSense: "",
+        favoriteOrCollect: "",
+        tpmSkill1: "",
+        tpmSkill2: "",
+        tpmSkill3: "",
+        tpmInterest1: 0,
+        tpmInterest2: 0,
+        tpmInterest3: 0,
+        tpmInterest4: 0,
+        uxInterest1: 0,
+        uxInterest2: 0,
+        frontendInterest1: 0,
+        frontendInterest2: 0,
+        backendInterest1: 0,
+        backendInterest2: 0,
+        dataEngInterest1: 0,
+        dataEngInterest2: 0,
+        dataEngInterest3: 0,
+        mlEngInterest1: 0,
+        mlEngInterest2: 0,
+        mlEngInterest3: 0,
+        mlOpsInterest1: 0,
+        mlOpsInterest2: 0,
+        mlOpsInterest3: 0,
+      }
     );
   });
 };
