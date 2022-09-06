cf480932c4b629d52923ead4a88573d7e9a7579f
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2022-09-06 08:35:20 -0600

    Update LabsApplication.ts

--- src/routes/LabsApplication.ts
+++ src/routes/LabsApplication.ts
@@ -46,7 +46,7 @@ export async function postLabsApplication(
   res: Response
 ): Promise<Response> {
   const labsApplicationSubmission = req.body as ILabsApplicationSubmission;
-  console.log(JSON.stringify(labsApplicationSubmission));
+  console.log('🪂 REQ.BODY 🪂', req.body);
   try {
     return res.json(await processLabsApplication(labsApplicationSubmission));
   } catch (error) {
