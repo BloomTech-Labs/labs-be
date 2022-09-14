238d19dba4422938900ba33ed53a7434abb7b4df
Ryan Hamblin <8798259+ryan-hamblin@users.noreply.github.com>
2022-09-14 16:45:52 -0600

    removes logs from project (#32)

--- src/daos/Salesforce/ContactDao.ts
+++ src/daos/Salesforce/ContactDao.ts
@@ -52,7 +52,6 @@ export default class ContactDao {
   ): Promise<LabsProject[]> {
     await this.client.login();
     for (const project of projects) {
-      console.log(project);
       const trackEnrollments = await this.client.connection.query<{
         Contact__r: { Okta_Id__c: string };
       }>(
@@ -60,10 +59,6 @@ export default class ContactDao {
           Select Contact__r.Okta_Id__c from JDS_Track_Enrollment__c Where Labs_Projects__r.Name In ('${project.id}')
         `
       );
-      console.log("TRACK ENROLLMENT IDs", JSON.stringify(trackEnrollments.records))
-      // filter out any nulls, 
-
-      // map over by OKtaID
       trackEnrollments.records = trackEnrollments.records.filter(record => record.Contact__r !== null);
       project.teamMemberSmtIds = trackEnrollments.records
         .map((record) => record.Contact__r["Okta_Id__c"]);
